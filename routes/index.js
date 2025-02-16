720d46a201787869f3c4b3e59cbee73ea20f2ef7
sabilar-rahman <sabilarrahman36@gmail.com>
2025-02-16 16:02:13 +0600

    route setup

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+import {Router} from "express";
+import UserRouters from "./userRoutes.js";
+
+const router = Router();
+
+router.use("/api/user", UserRouters);
+
+export default router;
--- server.js
+++ server.js
@@ -6,10 +6,22 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 
 app.get("/", (req, res) => {
   res.send("Hello Prisma!");
+});
+
+
+// route file
+import routes from "./routes/index.js"
+app.use(routes)
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
