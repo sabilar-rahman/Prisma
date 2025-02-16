5e47865d91f04b0f985cada60b44c7602d0c8d15
sabilar-rahman <sabilarrahman36@gmail.com>
2025-02-16 15:20:47 +0600

    user model schema create

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
 
 app.get("/", (req, res) => {
-    res.send("Hello Prisma!");
+  res.send("Hello Prisma!");
 });
